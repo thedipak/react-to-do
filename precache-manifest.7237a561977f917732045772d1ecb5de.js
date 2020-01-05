d1fea4e2d1466c10f5f297c06f44e858c61cc1cd
Dipak <deepakumar.dx@gmail.com>
2020-01-05 16:28:03 +0530

    Updates

---
+++ precache-manifest.7237a561977f917732045772d1ecb5de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "088a8af99bbabbbef61f1d0287505929",
+    "url": "/react-to-do/index.html"
+  },
+  {
+    "revision": "8e73b0010922408ee280",
+    "url": "/react-to-do/static/css/main.194b9cea.chunk.css"
+  },
+  {
+    "revision": "f07511a823b74688d9c1",
+    "url": "/react-to-do/static/js/2.58d53023.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-to-do/static/js/2.58d53023.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8e73b0010922408ee280",
+    "url": "/react-to-do/static/js/main.5a3d179f.chunk.js"
+  },
+  {
+    "revision": "07d936853f9a803c7dcf",
+    "url": "/react-to-do/static/js/runtime-main.4ece73db.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-to-do/static/media/logo.5d5d9eef.svg"
+  }
+]);
